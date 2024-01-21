d06050dfa335975804126efddb4119a1dd785b62
laxuanlinh <laxuanlong@gmail.com>
2024-01-21 18:24:19 +0800

    stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import { Amplify, Auth } from "aws-amplify";
+import { Hub } from "aws-amplify/utils";
+import { signInWithRedirect, signOut, getCurrentUser, fetchAuthSession } from "aws-amplify/auth";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "ap-southeast-1_uHIi7mkxG",
+      userPoolClientId: "5ii9mpg8qqo6tbibltlmv9f51k",
+      signUpVerificationMethod: "code",
+      loginWith: {
+        oauth: {
+          domain:
+            "linh-test.auth.ap-southeast-1.amazoncognito.com",
+          scopes: ["email", "openid"],
+          redirectSignIn: ["http://localhost:3000"],
+          redirectSignOut: ["http://localhost:3000"],
+          responseType: "token",
+        },
+      },
+    },
+  },
+});
+
+export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = Hub.listen("auth", ({ payload }) => {
+      switch (payload.event) {
+        case "signInWithRedirect":
+          getUser();
+          break;
+        case "signInWithRedirect_failure":
+          console.log("An error has ocurred during the Oauth flow.");
+          break;
+      }
+    });
+
+    getUser();
+
+    return unsubscribe;
+  }, []);
+
+  const getUser = async () => {
+    try {
+      const currentUser = await getCurrentUser();
+      setUser(currentUser);
+    } catch (error) {
+      console.error(error);
+      console.log("Not signed in");
+    }
+  };
+
+  return user !== null ? (<AuthenticatedApp user={user}></AuthenticatedApp>) : (<UnauthenticatedApp />);
+}
+
+function UnauthenticatedApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="App">
+        <button
+          onClick={
+            () =>
+              signInWithRedirect(
+                // sign in with federated user is not working yet
+                // { provider: { custom: "Okta" } }
+              )
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Log in
+        </button>
+      </div>
     </div>
   );
 }
 
-export default App;
+function AuthenticatedApp({ user }) {
+  
+  const [jwt, setJwt] = useState("");
+  const getToken = async () => {
+    const userAttributes = await fetchAuthSession();
+    setJwt(userAttributes.tokens.accessToken.toString());
+  }
+  return (
+    <div>
+      <button onClick={() => signOut()}>Sign Out</button>
+      <div>{user?.username}</div>
+      <button onClick={getToken}>Get token</button>
+      <div>{jwt}</div>
+    </div>
+  );
+}
